5118b922eef7b36ed9058a43caa90464cf90a699
Zhang Matt <mzhang@lcs-mba3155.lcs.on.ca>
2017-09-28 10:56:34 -0400

    Clean-up of commented-out code

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -26,25 +26,7 @@ let canvas = Canvas(width: 500, height: 500)
 PlaygroundPage.current.liveView = canvas.imageView
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-// Draw a circle at the origin with radius of 50 pixels
-//canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
 
-// Show where the origin is
-canvas.drawAxes()
-
-// Draw an ellipse in a different color at the centre of the canvas
-//canvas.fillColor = Color.green
-//canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
-
-//for i in 1...20
-//{
-//    canvas.drawLine(fromX: 0, fromY: 500, toX: i*25, toY: 0)
-//}
-//
-//for i in 1...20
-//{
-//    canvas.drawLine(fromX: 0, fromY: 0, toX: i*25, toY: 500)
-//}
 for i in 1...20
 {
     canvas.drawLine(fromX: 500, fromY: i*25, toX: 0, toY: 0)
